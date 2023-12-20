1f9e166e4227428ac686b338b8236a4e1cc0067b
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2023-12-20 00:14:59 -0300

    atualizando app

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,9 +6,9 @@ import { Botao } from './components/Botao';
 import { EntradaTexto } from './components/EntradaTexto';
 
 
-export default function Login() {
+export default function Login({ navigation }) {
 
-   return (
+    return (
         <VStack flex={1} alignItems="center" justifyContent="center" padding={5}>
             <Image source={Logo} alt='Logo Voll' />
             <Titulo>
@@ -18,11 +18,11 @@ export default function Login() {
                 <EntradaTexto label='Email' placeholder='Insira seu endereço de e-mail' />
                 <EntradaTexto label='Senha' placeholder='Insira sua senha' />
             </Box>
-            <Botao> Entrar </Botao>
+            <Botao onPress={() => navigation.navigate('Tabs')}> Entrar </Botao>
             <Link href='https://www.alura.com.br' marginTop={2}>Esqueceu sua senha?</Link>
             <Box w="100%" flexDirection="row" justifyContent="center" marginTop={5} >
                 <Text>Ainda não tem cadastro? </Text>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('Cadastro')}>
                     <Text color="blue.500"> Faça seu cadastro!</Text>
                 </TouchableOpacity>
             </Box>
