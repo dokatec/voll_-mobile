5bf46f7957aaee291a941d232bd17507fae68500
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2023-12-27 17:55:10 -0300

    atualizando meu app

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,9 +1,27 @@
-import { VStack, Text } from 'native-base';
+import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base';
+
+import { Titulo } from '../components/Titulo';
+
 
 export default function Perfil() {
     return (
-        <VStack>
-            <Text>Perfil</Text>
-        </VStack>
+        <ScrollView>
+            <VStack flex={1} alignItems="center" p={5}>
+                <Titulo color="blue.500">Meu Perfil</Titulo>
+
+                <Avatar size="xl" source={{ uri: "https://github.com/dokatec.png" }}
+                    mt={5} />
+                <Titulo color="blue.500">Informações pessoais</Titulo>
+                <Titulo fontSize="lg">Jhonatan Pacheco</Titulo>
+                <Text>19/07/1990</Text>
+                <Text>São Paulo</Text>
+
+                <Divider mt={5} />
+
+                <Titulo color="blue.500" marginBottom={1}>Histório médico</Titulo>
+                <Text>Bronquite</Text>
+                <Text>Sinusite</Text>
+            </VStack>
+        </ScrollView>
     )
 }
--- src/Tabs/Principal.tsx
+++ src/Tabs/Principal.tsx
@@ -1,9 +1,38 @@
-import { VStack, Text } from 'native-base';
+import { VStack, Text, ScrollView, Image, Divider, Box } from 'native-base';
+
+import { Titulo } from '../components/Titulo';
+import { EntradaTexto } from '../components/EntradaTexto';
+import { Botao } from '../components/Botao';
+import Logo from '../assets/Logo.png';
+import { depoimentos } from '../utils/mock';
 
 export default function Principal() {
     return (
-        <VStack>
-            <Text>Principal</Text>
-        </VStack>
+        <ScrollView flex={1} backgroundColor="white">
+            <VStack flex={1} alignContent='flex-start' alignItems='flex-start' p={5}>
+                <Image source={Logo} alt="Logo" mt={10} />
+                <Titulo color='blue.500'>Boas-vindas!</Titulo>
+                <Box width="100%" borderRadius="lg" p={3} mt={10} shadow="1" borderRightRadius="md" >
+                    <EntradaTexto placeholder='Digite a especialidade' />
+                    <EntradaTexto placeholder='Digite sua localização' />
+                    <Botao marginTop={3} marginBottom={3}>Buscar</Botao>
+                </Box>
+                <Titulo color='blue.800' alignSelf="center">Depoimentos</Titulo>
+                <VStack space={3} divider={<Divider />} width="100%">
+                    {
+                        depoimentos.map(depoimento => (<Box key={depoimento.id} width="100%"
+                            borderRadius="lg" p={3}>
+                            <Text color="gray.300" fontSize="md" textAlign="justify">
+                                {depoimento.text}
+                            </Text>
+                            <Text color="gray.500" fontSize="lg" fontWeight="bold"
+                                alignSelf="center" mt="2">
+                                {depoimento.titulo}
+                            </Text>
+                        </Box>))
+                    }
+                </VStack>
+            </VStack>
+        </ScrollView>
     )
 }
